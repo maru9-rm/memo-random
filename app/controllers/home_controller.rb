c34cc018f4c1b8cac798f013ffc595bbb88c9c7f
Ryota9 <ngm9dwf@gmail.com>
2022-05-06 11:47:49 +0900

    validationの設定とやり忘れていたedit機能の実装。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
     before_action :authenticate_user!
     def index
+        randoms = current_user.memorandums
+        # @random = randoms.order("RANDOM()").first
+        @random = randoms.sample
     end
 end
